acc7ba5a42dfd8e56cffd0bced490ba15d774cc2
Igor <103604423+LIS-CPP@users.noreply.github.com>
2023-03-19 17:37:41 +0500

    Create task6.cs

---
+++ solutions/task6.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Text;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+
+            int cols = 3;
+            int pictureCount = 52;
+            int rows = pictureCount / cols;
+            int leftovers = pictureCount % cols;
+
+            Console.WriteLine($"Полностью заполненых рядов: {rows}\n" +
+                $"Картинок сверх меры: {leftovers}\n");
+        }
+    }
+}
