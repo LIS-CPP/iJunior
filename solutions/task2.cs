39af1cfdd016a95acd98249217276039d6a06843
Igor <103604423+LIS-CPP@users.noreply.github.com>
2023-03-19 16:04:25 +0500

    Create task2.cs

---
+++ solutions/task2.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Igor";
+            ulong experience = 823675493485;
+            int customColor = 0x31337F;
+            byte ownedByPlayer = 7;
+            short power = 1400;
+            double targetPosition = 3.1415926;
+            long money = 450000;
+            float moveSpeed = 2.4f;
+            bool isAlive = true;
+            char hotkey = 'I';
+        }
+    }
+}
