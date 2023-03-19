19df6a078b74c1bdc63a9286666a5070cf06c273
Igor <103604423+LIS-CPP@users.noreply.github.com>
2023-03-19 18:26:28 +0500

    Update task6.cs

--- solutions/task6.cs
+++ solutions/task6.cs
@@ -9,13 +9,13 @@ namespace Program
         {
             Console.OutputEncoding = Encoding.Unicode;
 
-            int cols = 3;
-            int pictureCount = 52;
-            int rows = pictureCount / cols;
-            int leftovers = pictureCount % cols;
+            int picturesInRow = 3;
+            int picturesAmount = 52;
+            int filledRows = picturesAmount / picturesInRow;
+            int remainingPictures = picturesAmount % picturesInRow;
 
-            Console.WriteLine($"Полностью заполненых рядов: {rows}\n" +
-                $"Картинок сверх меры: {leftovers}\n");
+            Console.WriteLine($"Полностью заполненых рядов: {filledRows}\n" +
+                $"Картинок сверх меры: {remainingPictures}\n");
         }
     }
 }
