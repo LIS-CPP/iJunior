9b56f7986622340995415d74527cc5bde6799acd
Igor <103604423+LIS-CPP@users.noreply.github.com>
2023-03-19 18:44:36 +0500

    Create task8.cs

---
+++ solutions/task8.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Text;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+
+            const int delayTime = 10;
+            const int minutesInHour = 60;
+
+            Console.Write("Введите количество людей в очереди: ");
+
+            int peolpeAmount;
+            peolpeAmount = Convert.ToInt32(Console.ReadLine());
+
+            int timeToWait = peolpeAmount * delayTime;
+            int hoursToWait = timeToWait / minutesInHour;
+            int remainingMinutesToWait = timeToWait % minutesInHour;
+
+            Console.WriteLine($"Вам придётся отстоять в очереди {hoursToWait} часа" +
+                $"и {remainingMinutesToWait} минут");
+        }
+    }
+}
